afaa492c458185bf852aa0bdb2c092c1db1a6030
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-04-10 19:03:09 -0300

    Lista de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { Container, Title, List } from './styles';
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
   return (
     <Background>
-      <Text>Dashboard</Text>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
     </Background>
   );
 }
