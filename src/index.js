eb40eb695a2814d1a03c35a3c393d1bbfb550972
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-04-21 19:56:44 -0300

    Configurando Appcenter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { Component } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import '~/config/ReactotronConfig';
 
@@ -10,14 +12,44 @@ import { store, persistor } from '~/store';
 
 import App from '~/App';
 
-const Index = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <App />
-    </PersistGate>
-  </Provider>
-);
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    OneSignal.init('baef2452-4d0f-44d3-89d1-e4372331c38e');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  UNSAFE_componentWillMount() {
+    OneSignal.removeEventListener('received');
+    OneSignal.removeEventListener('opened');
+    OneSignal.removeEventListener('ids');
+  }
+
+  onReceived = data => {
+    console.log(data);
+  };
+
+  onOpened = notification => {
+    console.log(notification);
+  };
+
+  onIds = id => {
+    console.log(id);
+  };
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 export default CodePush({
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
